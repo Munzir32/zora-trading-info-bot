f0a74b7a43607dd0a7a444ef701536968770ffc7
Munzir32 <adeniranm07@gmail.com>
2025-04-10 18:31:31 +0000

    update code

--- src/api/webhook.ts
+++ src/api/webhook.ts
@@ -1,16 +1,16 @@
-
-import { bot } from "../src/bot";
+import { bot } from "../bot";
+import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
-  req,
-  res
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
     if (req.method === 'POST') {
       try {
         const update = req.body;
         await bot.processUpdate(update);
         res.status(200).json({ ok: true });
-      } catch (error) {
+      } catch (error: any) {
         console.error('Error handling update:', error);
         res.status(500).json({ ok: false, error: error.message });
       }
