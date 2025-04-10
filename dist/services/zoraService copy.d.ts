0bb4bdfc7ad561be9e98d52ecee4e74cb6844916
Munzir32 <adeniranm07@gmail.com>
2025-04-10 18:05:05 +0000

    fix code

--- dist/services/zoraService copy.d.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from 'ethers';
-export declare const createZoraService: (provider: ethers.JsonRpcProvider, apiKey: string) => {
-    getTokenPrice: (tokenId: string, contractAddress: string) => Promise<number>;
-    getTokenMarketData: (tokenId: string, contractAddress: string) => Promise<any>;
-    getTokenHistory: (tokenId: string, contractAddress: string) => Promise<any>;
-    getTokenMetadata: (tokenId: string, contractAddress: string) => Promise<any>;
-    generateTradingAnalysis: (tokenId: string, contractAddress: string) => Promise<any>;
-};
